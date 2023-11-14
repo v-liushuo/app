e446f2324fb39544cb027cf761afd545ed36f29e
liushuo <v-liushuo@foxmail.com>
2023-11-14 22:28:54 +0800

    update DynamicStateExample.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import FormEventExample from "@/components/FormEventExample.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 import GsapExample from "@/components/GreenSockExample.vue";
+import DynamicStateExample from "@/components/DynamicStateExample.vue";
 
 Vue.use(VueRouter)
 
@@ -27,6 +28,12 @@ const routes = [
         path: '/gsap',
         name: 'gsap',
         component: GsapExample
+    },
+
+    {
+        path: '/dynamic',
+        name: 'dynamic',
+        component: DynamicStateExample
     }
 ];
 const router = new VueRouter({
