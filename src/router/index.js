f4c8852435e8b146ace4cb3c67c018adb338aaf3
albert.liu <48754132+v-liushuo@users.noreply.github.com>
2024-01-19 11:21:59 +0800

    添加路由自动注册功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,88 +1,22 @@
 import Vue from "vue";
 import VueRouter from 'vue-router';
-import FormEventExample from "@/components/FormEventExample.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
-import GsapExample from "@/components/GreenSockExample.vue";
-import DynamicStateExample from "@/components/DynamicStateExample.vue";
-import DirectiveExample from "@/components/DirectiveExample.vue";
-import CSSExample from "@/components/CSSExample.vue";
-import CSSLearnExample from "@/components/CSSLeranExample.vue";
-import BoxLearnExample from "@/components/BoxLeranExample.vue"
-import TableFormatterExample from "@/components/TableFormatterExample.vue"
-import CanvasExample from "@/components/CanvasExample.vue";
-import CanvasV2Example from "@/components/CanvasV2/CanvasV2Example.vue";
-import DevilHole from "@/components/DevilHole/DevilHole.vue";
+
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: '/',
-        redirect: HelloWorld
-    },
-    {
-        path: '/form-event',
-        name: 'FormEventExample',
-        component: FormEventExample
-    },
+const routes = [];
+require
+    .context("../components", true, /\.vue$/)
+    .keys()
+    .map((item) => {
+        let path = item.slice(1).replace(".vue", "");
+        routes.push({
+            path,
+            name: path.slice(1),
+            component: () => import(`../components${item.slice(1)}`),
+        });
+    });
 
-    {
-        path: '/hello',
-        name: 'HelloWorld',
-        component: HelloWorld
-    },
-
-    {
-        path: '/gsap',
-        name: 'gsap',
-        component: GsapExample
-    },
-
-    {
-        path: '/dynamic',
-        name: 'dynamic',
-        component: DynamicStateExample
-    },
-    {
-        path: '/directive',
-        name: 'directive',
-        component: DirectiveExample
-    },
-    {
-        path: '/css',
-        name: 'css',
-        component: CSSExample
-    },
-    {
-        path: '/css-learn',
-        name: 'css-learn',
-        component: CSSLearnExample
-    },
-    {
-        path: '/box-learn',
-        name: 'box-learn',
-        component: BoxLearnExample
-    },
-    {
-        path: '/table-formatter',
-        name: 'table-formatter',
-        component: TableFormatterExample
-    },
-    {
-        path: '/canvas-example',
-        name: 'canvas-example',
-        component: CanvasExample
-    },
-    {
-        path: '/canvas-v2-example',
-        name: 'canvas-v2-example',
-        component: CanvasV2Example
-    },
-    {
-        path: '/devil-hole',
-        name: 'devil-hole',
-        component: DevilHole
-    }
-];
+console.log(routes)
 const router = new VueRouter({
     routes: routes
 });
