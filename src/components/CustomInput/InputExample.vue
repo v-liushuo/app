040ebfb187d842dfb0c078cb522289da9ca9ac2d
liushuo <v-liushuo@foxmail.com>
2024-02-29 07:12:34 +0800

    update some stuff

---
+++ src/components/CustomInput/InputExample.vue
@@ -0,0 +1,24 @@
+<script>
+import CustomInput from "@/components/CustomInput/CustomInput.vue";
+
+export default {
+  name: "InputExample",
+  components: {CustomInput},
+  data() {
+    return {
+      searchText: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <CustomInput v-model="searchText"></CustomInput>
+    <p>{{ searchText }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
